97b1cd5a8ad0dce48c49d8580b47857728996001
Siomara Nieves <snieves2014@my.fit.edu>
2017-10-10 18:11:10 -0700

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,4 +1,4 @@
-//Read File 'Hello World' Test on Python
+#Read File 'Hello World' Test on Python
 import csv
 with open('test.csv', 'rb') as csvfile:
     dataReader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
