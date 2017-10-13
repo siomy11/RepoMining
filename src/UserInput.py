7035c09a9ffbd4372c3d99e378155a7f94203269
Jessica Nguy <jnguy2014@my.fit.edu>
2017-10-13 11:57:33 -0400

    Create UserInput.py

---
+++ src/UserInput.py
@@ -0,0 +1,5 @@
+# Python code for UserInput
+# Currently prompts user for input and displays input back.
+
+targetVariable = (input("Select Target Variable: "))
+print("The Target Variable is:", targetVariable)
