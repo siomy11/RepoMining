8bc4a993f8194a68594eb8c5c56b0a79576c5f6d
Siomara Nieves <snieves2014@my.fit.edu>
2017-10-21 17:09:11 -0700

    Update csv.py

--- src/csv.py
+++ src/csv.py
@@ -1,6 +1,84 @@
-#Read File 'Hello World' Test on Python
 import csv
-with open('test.csv', 'rb') as csvfile:
-    dataReader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
-    for row in dataReader:
-        print(row)
+import scipy
+import numpy as np
+import plotly as py
+import plotly.graph_objs as go
+
+#things to do
+
+#either have the provider have the names for data as "DATE" and "DATA"
+#or have python know that if its a string then skip it, tried but then read all
+#numbers as strings
+
+#the test file being used has dates with no data, where they put a period
+
+#maybe if there's also blank space just skip it
+
+
+np.set_printoptions(threshold=np.inf)
+with open('test.csv') as csvfile: #for now just reading it from my computer
+    readCSV = csv.reader(csvfile, delimiter = ',') 
+    
+    dates = []
+    info = []
+    
+    for row in readCSV:
+        time = row[0]
+        data = row[1]
+
+        if time == "." or data == ".":   #where there's a date with no data put ' . '
+            continue
+        
+        elif time == "DATE" and data == "DATA": #assuming first row has date and date strings, check above
+            continue
+            
+        elif time == " " or data == " ": #testing for blanks
+            continue
+            
+        else:                           #only add them in here
+            dates.append(time)
+            info.append(data)
+            
+    dates = list(map(float,dates))      #change all strings into floats 
+    info = list(map(float,info))
+    
+    arraydates = np.asarray(dates)   #change list into array
+    arrayinfo = np.asanyarray(info)  
+    
+    z_score = []
+    
+    if len(dates) == len(info):       #do Q1
+        zscore = (scipy.stats.zscore(arrayinfo)) #calculate zscore
+        
+        trace = go.Scatter(  #ignore this just playing around 
+                x = dates,
+                y = zscore,
+                line = dict(
+                        color = ('rgb(205,12,24)'),
+                        width = 4
+                        )
+        )
+        
+        data = [trace]
+        
+
+    else:
+        print("columns are not of same length")
+        
+        
+        
+        
+
+   
+  
+
+
+            
+            
+    
+
+            
+  
+            
+        
+    
